ee9e2ae49439b2410b0f1552d3f48464a39991cd
englmaxi <englmaxi@live.com>
2019-05-17 01:52:34 +0200

    implemented the "Memory Unit"

--- src/memu.vhd
+++ src/memu.vhd
@@ -20,5 +20,148 @@ end memu;
 architecture rtl of memu is
 
 begin  -- rtl
+	M_byteena_wrdata_OUT : process(all)
+		variable BB, AA, XX : std_logic_vector(BYTE_WIDTH-1 downto 0);
+	begin
+		BB := W(2*BYTE_WIDTH-1 downto BYTE_WIDTH);
+		AA := W(BYTE_WIDTH-1 downto 0);
+		XX := (others => '-');
 
+		case op.memtype is
+			when MEM_B | MEM_BU =>
+				case A(1 downto 0) is
+					when "00" =>
+						M.byteena <= "1000";
+						M.wrdata <= AA & XX & XX & XX;
+					when "01" =>
+						M.byteena <= "0100";
+						M.wrdata <= XX & AA & XX & XX;
+					when "10" =>
+						M.byteena <= "0010";
+						M.wrdata <= XX & XX & AA & XX;
+					when "11" =>
+						M.byteena <= "0001";
+						M.wrdata <= XX & XX & XX & AA;
+					when others => null;
+				end case;
+
+			when MEM_H | MEM_HU =>
+				case A(1 downto 0) is
+					when "00" | "01" =>
+						M.byteena <= "1100";
+						M.wrdata <= BB & AA & XX & XX;
+					when "10" | "11" =>
+						M.byteena <= "0011";
+						M.wrdata <= XX & XX & BB & AA;
+					when others => null;
+				end case;
+
+			when MEM_W =>
+				case A(1 downto 0) is
+					when "00" | "01" | "10" | "11" =>
+						M.byteena <= "1111";
+						M.wrdata <= W; -- W == DCBA
+					when others => null;
+				end case;
+
+		end case;
+	end process;
+
+	R_OUT : process(all)
+		variable DD, CC, BB, AA, SS, zero : std_logic_vector(BYTE_WIDTH-1 downto 0);
+	begin
+		DD := D(4*BYTE_WIDTH-1 downto 3*BYTE_WIDTH);
+		CC := D(3*BYTE_WIDTH-1 downto 2*BYTE_WIDTH);
+		BB := D(2*BYTE_WIDTH-1 downto BYTE_WIDTH);
+		AA := D(BYTE_WIDTH-1 downto 0);
+		SS := (others => '0');
+		zero := (others => '0');
+
+		case op.memtype is
+			when MEM_B =>
+				case A(1 downto 0) is
+					when "00" =>
+						SS := (others => DD(BYTE_WIDTH-1));
+						R <= SS & SS & SS & DD;
+					when "01" =>
+						SS := (others => CC(BYTE_WIDTH-1));
+						R <= SS & SS & SS & CC;
+					when "10" =>
+						SS := (others => BB(BYTE_WIDTH-1));
+						R <= SS & SS & SS & BB;
+					when "11" =>
+						SS := (others => AA(BYTE_WIDTH-1));
+						R <= SS & SS & SS & AA;
+					when others => null;
+				end case;
+
+			when MEM_BU =>
+				case A(1 downto 0) is
+					when "00" =>
+						R <= zero & zero & zero & DD;
+					when "01" =>
+						R <= zero & zero & zero & CC;
+					when "10" =>
+						R <= zero & zero & zero & BB;
+					when "11" =>
+						R <= zero & zero & zero & AA;
+					when others => null;
+				end case;
+
+			when MEM_H =>
+				case A(1 downto 0) is
+					when "00" | "01" =>
+						SS := (others => DD(BYTE_WIDTH-1));
+						R <= SS & SS & DD & CC;
+					when "10" | "11" =>
+						SS := (others => BB(BYTE_WIDTH-1));
+						R <= SS & SS & BB & AA;
+					when others => null;
+				end case;
+
+			when MEM_HU =>
+				case A(1 downto 0) is
+					when "00" | "01" =>
+						R <= zero & zero & DD & CC;
+					when "10" | "11" =>
+						R <= zero & zero & BB & AA;
+					when others => null;
+				end case;
+
+			when MEM_W =>
+				case A(1 downto 0) is
+					when "00" | "01" | "10" | "11" =>
+						R <= D; -- D == DCBA
+					when others => null;
+				end case;
+
+		end case;
+	end process;
+
+	XL_XS_adress_rd_wr_OUT : process(all)
+	begin
+		M.address <= A;
+		M.rd <= op.memread;
+		M.wr <= op.memwrite;
+		XL <= '0';
+		XS <= '0';
+
+		if (op.memread = '1' and A(1 downto 0) = "00" 		and  A(ADDR_WIDTH-1 downto 2) = (ADDR_WIDTH-3 downto 0 => '0')) 		or
+			 (op.memread = '1' and op.memtype 	 = MEM_H 		and (A(1 downto 0) = "01" or A(1 downto 0) = "11")) or
+			 (op.memread = '1' and op.memtype 	 = MEM_HU		and (A(1 downto 0) = "01" or A(1 downto 0) = "11")) or
+			 (op.memread = '1' and op.memtype 	 = MEM_W	 	and (A(1 downto 0) = "01" or A(1 downto 0) = "10" 	or A(1 downto 0) = "11")) then
+				 XL <= '1';
+				 M.rd <= '0';
+				 M.wr <= '0';
+		end if;
+
+		if (op.memwrite = '1' and A(1 downto 0) = "00"		and  A(ADDR_WIDTH-1 downto 2) = (ADDR_WIDTH-3 downto 0 => '0'))		or
+			 (op.memwrite = '1' and op.memtype 		= MEM_H		and (A(1 downto 0) = "01" or A(1 downto 0) = "11")) or
+			 (op.memwrite = '1' and op.memtype 		= MEM_HU 	and (A(1 downto 0) = "01" or A(1 downto 0) = "11")) or
+			 (op.memwrite = '1' and op.memtype 		= MEM_W 	and (A(1 downto 0) = "01" or A(1 downto 0) = "10" 	or A(1 downto 0) = "11")) then
+				 XS <= '1';
+				 M.rd <= '0';
+				 M.wr <= '0';
+		end if;
+	end process;
 end rtl;
