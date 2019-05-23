fc53d63faa07e6132dbdbac0ca3217574b5e40c0
englmaxi <englmaxi@live.com>
2019-05-23 15:07:02 +0200

    made flush independent of stall

--- src/wb.vhd
+++ src/wb.vhd
@@ -30,13 +30,15 @@ begin  -- rtl
 			rd_out <= (others => '0');
 			result <= (others => '0');
 			regwrite <= '0';
-		elsif rising_edge(clk) and stall = '0' then
-			regwrite <= op.regwrite;
-			rd_out <= rd_in;
-			if op.memtoreg = '1' then
-				result <= memresult;
-			else
-				result <= aluresult;
+		elsif rising_edge(clk) then
+			if stall = '0' then
+				regwrite <= op.regwrite;
+				rd_out <= rd_in;
+				if op.memtoreg = '1' then
+					result <= memresult;
+				else
+					result <= aluresult;
+				end if;
 			end if;
 			if flush = '1' then
 				rd_out <= (others => '0');
