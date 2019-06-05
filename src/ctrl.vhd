0af91e46247d138e5eee469f5065894841982f51
TheBitFighter <flip97.fp@gmail.com>
2019-06-05 15:59:29 +0200

    Implemented basic control unit

--- src/ctrl.vhd
+++ src/ctrl.vhd
@@ -6,15 +6,41 @@ use work.core_pack.all;
 use work.op_pack.all;
 
 entity ctrl is
-	
+
 	port (
-		-- define input and output ports as needed
+		clk : in std_logic;
+		reset : in std_logic;
+		op : in exec_op_type;
+		fl_fetch : out std_logic;
+		fl_decode : out std_logic
 );
 
 end ctrl;
 
 architecture rtl of ctrl is
 
-begin  -- rtl
+	signal current_op : exec_op_type;
+
+begin
+
+	latch : process(clk, reset)
+	begin
+		if (reset = '1') then
+		elsif (rising_edge(clk)) then
+			current_op <= op;
+		end if;
+	end process;
+
+	flush : process(current_op)
+	begin
+		if (current_op.branch = '1') then
+			fl_fetch <= '1';
+			fl_decode <= '1';
+		else
+			fl_fetch <= '0';
+			fl_decode <= '0';
+		end if;
+
+	end process;
 
 end rtl;
