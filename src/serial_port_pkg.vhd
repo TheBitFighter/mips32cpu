926601596649fd3bd60bca2361e5942e1c0cafef
englmaxi <englmaxi@live.com>
2019-06-14 14:56:42 +0200

    fixed syntax

--- src/serial_port_pkg.vhd
+++ src/serial_port_pkg.vhd
@@ -4,25 +4,25 @@ use ieee.numeric_std.all;
 
 package serial_port_pkg is
   component serial_port is
-    generic map(
+    generic(
       clk_freq : integer;
       baud_rate : integer;
       sync_stages : integer;
       tx_fifo_depth : integer;
       rx_difo_depth : integer
-    )
+    );
     port(
       clk : in std_logic;
       res_n : in std_logic;
-      rx : in std_logic;
-      tx : in std_logic;
       tx_data : in std_logic_vector(7 downto 0);
       tx_wr : in std_logic;
       tx_free : out std_logic;
-      tx_rd : in std_logic;
       rx_data : out std_logic_vector(7 downto 0);
+      rx_rd : in std_logic;
       rx_empty : out std_logic;
       rx_full : out std_logic
+      rx : in std_logic;
+      tx : in std_logic
     );
   end component;
 end package;
